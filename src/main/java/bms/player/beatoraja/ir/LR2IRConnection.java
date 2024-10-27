bcb57cf7071af9d59ac74b7c4f852274c96019af
Sayaka <alice@lolita.technology>
2024-10-27 11:42:35 +0100

    update version in LR2IRConnection

--- src/main/java/bms/player/beatoraja/ir/LR2IRConnection.java
+++ src/main/java/bms/player/beatoraja/ir/LR2IRConnection.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 public class LR2IRConnection implements IRConnection {
     public static final String NAME = "LR2IR Read Only";
     public static final String HOME = "";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     private static final String IRUrl = "http://dream-pro.info/~lavalse/LR2IR/2";
 
